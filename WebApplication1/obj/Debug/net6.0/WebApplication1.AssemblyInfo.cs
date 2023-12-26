35d055e6df9950bd7bb0d49b3b2f9a85873834cc
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2023-12-26 10:10:01 -0500

    Change to Create.cshtml

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43cb40c968bf34d5b5aca17f4a9fbe26018ab72c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e4aacfe14d3e7217878115948b3e689dbfed10")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
