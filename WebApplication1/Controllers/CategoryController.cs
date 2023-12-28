53404093a102c04e2f9930186cb9b73c6fa839c1
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2023-12-28 10:08:12 -0500

    3.23 mark

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -27,11 +27,68 @@ namespace WebApplication1.Controllers
 
             if (ModelState.IsValid) //Go to category.cs and examine the validation
             {
-                _db.Categories.Add(obj); //Add the object 
+                _db.Categories.Add(obj); //Add the new category using EF core
                 _db.SaveChanges(); //Save changes to the DB
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id ==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //If entity with the given primary key is found, return, otherwise null is returned 
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id); //LINQ method "FoD" to find the first category that matches the given Id. 
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault(); 
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost] //Post the Added Category to DB
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid) //Go to category.cs and examine the validation
+            {
+                _db.Categories.Update(obj); //Update the category using EF Core
+                _db.SaveChanges(); //Save changes to the DB
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //If entity with the given primary key is found, return, otherwise null is returned 
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")] 
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges(); //Save changes to the DB
+            return RedirectToAction("Index");
+
         }
     }
 }
--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d31bbcdcf938638c74c62408e8909ce50e32ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecdc8c205614a2574a1f7e7513387aff6100d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
